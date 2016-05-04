6a7e4545d59118959a268b74b65cfa25cd70e35e
er3456qi <er3456qi@outlook.com>
2016-05-04 10:40:28 +0800

    from_envvar

--- flaskr.py
+++ flaskr.py
@@ -18,7 +18,15 @@ from_object() 会查看给定的对象（如果该对象是一个字符串就会
 搜索对象中所有变量名均为大字字母的变量。在我们的应用中，已经将配置写在前面了。
 你可以把这些配置放到一个独立的文件中。
 """
-app.config.from_object(__name__)
+# app.config.from_object(__name__)
+
+"""
+通常，从一个配置文件中导入配置是比较好的做法，
+我们使用 from_envvar() 来完成这个工作，把上面的 from_object() 一行替换为下面这行。
+这样做就可以设置一个 FLASKR_SETTINGS 的环境变量来指定一个配置文件，并根据该文件来重载缺省的配置。
+silent 开关的作用是告诉 Flask 如果没有这个环境变量 不要报错。
+"""
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
